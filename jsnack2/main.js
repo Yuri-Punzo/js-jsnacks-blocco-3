76b594829f326bdb51363b337dd625c50a88a89b
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-21 12:21:34 +0200

    -

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -3,10 +3,13 @@ A partire da un array di stringhe, crea un secondo array formattando le stringhe
 Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
 
 const stringsArray = ["pippo", "PLUTO", "PaPerIno"]
+console.log(stringsArray);
 
 
 const newArray = stringsArray.map(myFunction)
 
-function myFunction() {
-    
+function myFunction(element) {
+     return element.toLowerCase()
 }
+
+console.log(newArray);
