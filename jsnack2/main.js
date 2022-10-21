26028a0a3b6b8c79502967dd6541fac490f4a879
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-21 12:02:43 +0200

    intial commit

---
+++ jsnack2/main.js
@@ -0,0 +1,12 @@
+/* Snack 2
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+const stringsArray = ["pippo", "PLUTO", "PaPerIno"]
+
+
+const newArray = stringsArray.map(myFunction)
+
+function myFunction() {
+    
+}
